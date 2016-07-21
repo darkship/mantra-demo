32fbcc9fb30ef424ef28edd476354beebb9fbe75
darkship <crumble_tk@hotmail.com>
2016-07-21 21:11:30 +0200

    tryed to add event test but doen't work

--- client/modules/core/components/tests/input.js
+++ client/modules/core/components/tests/input.js
@@ -1,15 +1,28 @@
 const {describe, it} = global;
-import {expect} from 'chai';
+import {expect,spy} from 'chai';
+
 import {shallow} from 'enzyme';
 import Input from '../input';
+import TestUtils from 'react-addons-test-utils'
 
 describe('core.components.input', () => {
-  it('created component with props',()=>{
-  	const props={type:"text",className:"myClass",value:"this is my text"};
+	const props={type:'text',className:'myClass',value:'this is my text'};
   	const el = shallow(<Input {...props} />);
+  it('created component with props',()=>{
   	expect(el.find('input')).to.have.length(1)
-  	expect(el.find('input').props().value).to.equal("this is my text");	
-  	expect(el.find('input').props().className).to.equal("myClass");	
-  	expect(el.find('input').props().type).to.equal("text");	
+  	expect(el.find('input').props().value).to.equal('this is my text');	
+  	expect(el.find('input').props().className).to.equal('myClass');	
+  	expect(el.find('input').props().type).to.equal('text');	
+  });
+
+  xit('onChange',(done)=>{
+  	const onChange = (v)=>{
+  		expect(v).to.equal('abc');
+  		done();
+  	};
+  	const props={type:'text',value:'abc',onChange};
+  	el.setProps(props);
+
+  	TestUtils.Simulate.change(el);//doen't work ?
   });
 });
