5bb5585443b0891d3688a9a140a7c90ee2ab3209
darkship <crumble_tk@hotmail.com>
2016-07-21 20:23:17 +0200

    change Input to React component for using events

--- client/modules/core/components/input.jsx
+++ client/modules/core/components/input.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Input = (props) => (
-  <input {...props}/>
-);
+
+class Input extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    onChange(e){
+    	this.props.onChange(e.target.value)
+    }
+    render() {
+        return (<input {...this.props} onChange={this.onChange.bind(this)}/>);
+    }
+}
 
 export default Input;
